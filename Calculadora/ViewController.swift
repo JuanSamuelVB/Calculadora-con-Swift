92ac4572335a05c8b47504d45570612119d6e5ac
isc <isc@linked-macos-sierra-2.shared>
2016-11-25 14:58:54 -0800

    Add operations

--- Calculadora/ViewController.swift
+++ Calculadora/ViewController.swift
@@ -23,5 +23,50 @@ class ViewController: UIViewController {
             userIsInTheMiddleOfTypingANumber = true
         }
     }
+    
+    @IBAction func operate(_ sender: UIButton) {
+        let operation = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        switch operation {
+        case "x": performOperatione { $0 * $1 }
+        case "÷": performOperatione { $0 * $1 }
+        case "+": performOperatione { $0 * $1 }
+        case "−": performOperatione { $0 * $1 }
+        case "√": performOperatione { sqrt($0) }
+        default: break
+        }
+    }
+    
+    private func performOperatione(operation: (Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    private func performOperatione(operation: (Double) -> Double) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    var operandStack = Array<Double>()
 
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        operandStack.append(displayValue)
+    }
+    
+    var displayValue: Double {
+        get {
+            return NumberFormatter().number(from: display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingANumber = false
+        }
+    }
 }
